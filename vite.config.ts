fc0ca415baa5fd2345a6bb3d2d5a9a389bd471c3
heyxmirko <lukasektomas1@seznam.cz>
2024-09-16 21:59:50 +0200

    Including assets to the build

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,31 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { copy } from 'vite-plugin-copy'
 
 export default defineConfig({
-  // prevent vite from obscuring rust errors
   clearScreen: false,
-  // Tauri expects a fixed port, fail if that port is not available
   server: {
     strictPort: true,
   },
-  // to access the Tauri environment variables set by the CLI with information about the current target
   envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
   build: {
-    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
     target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
-    // don't minify for debug builds
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'), // Make sure your entry file is correct
+        terminal: resolve(__dirname, 'terminal.html'),
+      }
+    }
   },
+  plugins: [
+    copy({
+      targets: [
+        { src: 'terminal.js', dest: 'dist' },
+        { src: 'assets/*', dest: 'dist/assets' }
+      ],
+      hook: 'writeBundle',
+    })
+  ]
 })
